3258d5387b522813c326bc03461457b71bc52d32
Matthew Mcvicker <23mcvicker@gmail.com>
2018-04-06 14:44:48 -0700

    Participation

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,17 +3,16 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-
+employees <- paste("Employee", 1:100)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+random2017 <- runif(100,40000, 50000)
 
 # Create a vector of 100 salaries in 2018 that have changed from 2017
 # Use `runif()` to add a random number between -5000 and 10000 to each of 2017's
 # salaries (the negative number means that a salary may have decreased!)
-
-
+random2018 <- runif(100, )
 # Create a data frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
 
